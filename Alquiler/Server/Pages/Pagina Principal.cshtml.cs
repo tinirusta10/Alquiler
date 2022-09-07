f6377fe159d40493f5cf15fe7b490fe3f70e5fb8
tinirusta10 <martinirusta10@hotmail.com>
2022-09-07 12:38:50 -0300

    pagina principal y producto

---
+++ Alquiler/Server/Pages/Pagina Principal.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Alquiler.Server.Pages
+{
+    public class Pagina_PrincipalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Alquiler/Server/Pages/Producto.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Alquiler.Server.Pages
+{
+    public class ProductoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
