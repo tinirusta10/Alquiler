d8e7d4a056188f79a0eae11fd308ee1a14c792ea
santiago moyano ministro <san_moyano@hotmail.com>
2022-09-23 23:33:12 -0300

    Creando conexion

---
+++ Alquiler/Client/Servicios/Httprespuesta.cs
@@ -0,0 +1,21 @@
+﻿namespace Alquiler.Client.Servicios
+{
+    public class Httprespuesta<T> //T = cualquier clase
+    {
+        public T Respuesta { get; }
+        public bool Error { get; }
+        public HttpResponseMessage HttpResponseMessage { get; }
+
+        public Httprespuesta(T respuesta,
+                             bool error,
+                             HttpResponseMessage httpResponseMessage)
+        
+        {
+            Respuesta = respuesta;
+            Error = error;
+            HttpResponseMessage = httpResponseMessage;
+        }
+
+        
+    }
+}
