ec700e8e55d1ea9b78f6287694689727fa250b8c
tinirusta10 <martinirusta10@hotmail.com>
2022-10-02 12:27:11 -0300

    coment

--- Alquiler/Client/Servicios/HttpService.cs
+++ Alquiler/Client/Servicios/HttpService.cs
@@ -2,34 +2,34 @@
 
 namespace Alquiler.Client.Servicios
 {
-    //public class HttpService
-    //{
-        //private readonly HttpClient http;
+    public class HttpService
+    {
+        private readonly HttpClient http;
 
-        //public HttpService(HttpClient http)
-        //{
-          //  this.http = http;
-        //}
+        public HttpService(HttpClient http)
+        {
+            this.http = http;
+        }
 
         //public async Task<Httprespuesta<T>> Get<T>(string url)
         //{
-            //var response = await http.GetAsync(url);
-            //if (response.IsSuccessStatusCode)
-            //{
-                //var respuesta = await deserealizarRespuesta<T>(response);
-              //  return new Httprespuesta<T>(respuesta, false, response);
-            //}
-            //else
-            //{
+        //    var response = await http.GetAsync(url);
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        var respuesta = await deserealizarRespuesta<T>(response);
+        //        return new Httprespuesta<T>(respuesta, false, response);
+        //    }
+        //    else
+        //    {
 
-          //  }
+        //    }
         //}
 
-        //private async Task<T> deserealizarRespuesta<T>(HttpResponseMessage response)
-       // {
-            //var respuestaStr = await response.Content.ReadAsStringAsync();
-          //  return JsonSerializer.Deserialize<T>(respuestaStr,
-        //        new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
-      //  }
-  //  }
+        private async Task<T> deserealizarRespuesta<T>(HttpResponseMessage response)
+        {
+            var respuestaStr = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(respuestaStr,
+                new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+        }
+    }
 }
